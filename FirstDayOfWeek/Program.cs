288cb2dfb98d95f4e6f223bfb7b139aa031907b8
Opilane TTHK <opilane@tthk.ee>
2020-10-28 16:03:37 +0200

    first commit

---
+++ FirstDayOfWeek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstDayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
